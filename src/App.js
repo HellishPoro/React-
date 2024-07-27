8112a1dac968a0cdf089091fc278e6c34bd8550f
Leonid <borl99@inbox.ru>
2024-07-27 19:53:15 +0300

    Итоговое обновление

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import './index.css';
+import styles from './app.module.css'
+
+
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	let [value = '', setValue] = useState()
+	let [list = [], setList] = useState()
+	let [error = '', setError] = useState()
+	const onInputButtonClick = () => {
+		let promptValue = prompt('Введите значение')
+		if (promptValue === null) {
+			return false
+		}
+		if (promptValue.length <= 2) {
+			setError(error = 'Введенное значение должно содержать минимум 3 символа ')
+		} else {
+			value = promptValue
+			error = ''
+		}
+		if (error !== '') {
+			promptValue = value
+		}
+
+		setValue(value = promptValue)
+	}
+	const isValueVaild = () => {
+		if (value.length <= 2) {
+			return false
+		} else {
+			return true
+		}
+	}
+
+
+	const onAddButtonClick = () => {
+		if (value.length < 2) {
+			list.push(value)
+		}
+		let id = Date.now()
+		const updatedList = [...list, { id, value }]
+		setList(updatedList)
+		setError('')
+		setValue('')
+	}
+
+
+	return (
+		<div className={styles.app}>
+			<h1 className={styles.pageHeading}>Ввод значения</h1>
+			<p className={styles.noMarginText}>
+				Текущее значение <code>value</code>: "<output className={styles.currentValue}>{value}</output>"
+			</p>
+			<div className={styles.error}>{error}</div>
+			<div className={styles.buttonsContainer}>
+				<button className={styles.button} onClick={onInputButtonClick}>Ввести новое</button>
+				<button className={styles.button} onClick={onAddButtonClick} disabled={!isValueVaild()} >Добавить в список</button>
+			</div>
+			<div className={styles.listContainer}>
+				<h2 className={styles.listHeading}>Список:</h2>
+				<p className={styles.noMarginText}>Нет добавленных элементов</p>
+				<ul className={styles.list}>{list.map(({ id, value }) =>
+					(<li className={styles.listItem} key={id}>{value}</li>))}
+				</ul>
+			</div>
+		</div>
+	);
+
 }
 
 export default App;
